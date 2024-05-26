f4556c6ae3dd45c5bd64687b656e0725c9254c61
Carlos Morais Blanco <carlosmoraisblanco@gmail.com>
2024-05-26 16:10:14 +0200

    todo

--- manageIt/src/main/java/controller/mejora/ControllerMenuLateralEmpresa.java
+++ manageIt/src/main/java/controller/mejora/ControllerMenuLateralEmpresa.java
@@ -1,11 +1,20 @@
 package controller.mejora;
 
+import controller.ControllerLogin;
 import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import modelo.CambiarIdioma;
 import modelo.Data;
+
+import java.io.IOException;
 
 public class ControllerMenuLateralEmpresa {
 
@@ -35,8 +44,23 @@ public class ControllerMenuLateralEmpresa {
     private Data data;
 
     @FXML
-    void cerrar(MouseEvent event) {
+    void cerrar(MouseEvent event) throws IOException {
+        this.data.setEmpresaSeleccionada(null);
 
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vista/login.fxml"), CambiarIdioma.getInstance().getBundle());
+        Parent root = fxmlLoader.load();
+        ControllerLogin controllerLogin = fxmlLoader.getController();
+        controllerLogin.recibirData(this.data);
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.getIcons().add(new Image("file:src/main/resources/images/menuLateral/logo.png"));
+        stage.show();
+        HBox hBox = (HBox) event.getSource();
+        Stage stage1 = (Stage) hBox.getScene().getWindow();
+        stage1.close();
     }
 
     @FXML
@@ -74,9 +98,9 @@ public class ControllerMenuLateralEmpresa {
 
         this.limpiarClasesImagenes();
 
-        this.imagenPanel.getStyleClass().add("panel");
+        this.imagenPanel.getStyleClass().add("empresa");
         this.imagenSalir.getStyleClass().add("cerrar");
-        this.imagenProyectos.getStyleClass().add("proyectos");
+        this.imagenProyectos.getStyleClass().add("usuarios");
         this.imagenAjustes.getStyleClass().add("config");
     }
 
@@ -85,7 +109,7 @@ public class ControllerMenuLateralEmpresa {
      */
     public void iniciarPanel(){
         this.hboxPanel.pseudoClassStateChanged(PseudoClass.getPseudoClass("selected"),true);
-        this.imagenPanel.getStyleClass().add("panelPresionado");
+        this.imagenPanel.getStyleClass().add("empresaPresionado");
 
     }
 
